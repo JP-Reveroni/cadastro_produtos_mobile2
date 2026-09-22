e74c9322808c81ed372377758a1a05c68f512358
João Pedro Reverone <Joaopedroreverone@gmail.com>
2026-09-22 11:50:07 -0300

    Codigo do Projeto
    Adicionado o Script do projeto de listagem de produtos:
    main
    Screen
    Model
    Service

--- cadastro_produtos/lib/main.dart
+++ cadastro_produtos/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cadastro_produtos/screens/home_screen.dart';
 
 void main() {
-  runApp(());
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+      debugShowCheckedModeBanner: false,
+    )
+  );
 }
